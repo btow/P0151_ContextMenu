152f25d93fdd681225a4a6d8d97c02ef27fe69bf
btow <btow@yandex.ru>
2016-12-28 16:08:05 +0500

    The context menu for the text elements of the interface and handler for them is replaced by a variant with xml.

--- app/src/main/java/com/example/samsung/p0151_contextmenu/MainActivity.java
+++ app/src/main/java/com/example/samsung/p0151_contextmenu/MainActivity.java
@@ -12,13 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvOutColor, tvOutSize;
 
-    final int MENU_COLOR_RED = 1;
-    final int MENU_COLOR_GREEN = 2;
-    final int MENU_COLOR_BLUE = 3;
-
-    final int MENU_SIZE_22 = 4;
-    final int MENU_SIZE_26 = 5;
-    final int MENU_SIZE_30 = 6;
+//    final int MENU_COLOR_RED = 1;
+//    final int MENU_COLOR_GREEN = 2;
+//    final int MENU_COLOR_BLUE = 3;
+//
+//    final int MENU_SIZE_22 = 4;
+//    final int MENU_SIZE_26 = 5;
+//    final int MENU_SIZE_30 = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +38,20 @@ public class MainActivity extends AppCompatActivity {
 
         switch (v.getId()) {
             case R.id.tvOutColor :
-                menu.add(0, MENU_COLOR_RED, 0, getString(R.string.red));
-                menu.add(0, MENU_COLOR_GREEN, 0, getString(R.string.green));
-                menu.add(0, MENU_COLOR_BLUE, 0, getString(R.string.blue));
+                getMenuInflater().inflate(R.menu.menu_context_tv, menu);
+                menu.setGroupVisible(R.id.group_color, true);
+                menu.setGroupVisible(R.id.group_size, false);
+//                menu.add(0, MENU_COLOR_RED, 0, getString(R.string.red));
+//                menu.add(0, MENU_COLOR_GREEN, 0, getString(R.string.green));
+//                menu.add(0, MENU_COLOR_BLUE, 0, getString(R.string.blue));
                 break;
             case R.id.tvOutSize :
-                menu.add(0, MENU_SIZE_22, 0, R.string.size_22);
-                menu.add(0, MENU_SIZE_26, 0, R.string.size_26);
-                menu.add(0, MENU_SIZE_30, 0, R.string.size_30);
+                getMenuInflater().inflate(R.menu.menu_context_tv, menu);
+                menu.setGroupVisible(R.id.group_color, false);
+                menu.setGroupVisible(R.id.group_size, true);
+//                menu.add(0, MENU_SIZE_22, 0, R.string.size_22);
+//                menu.add(0, MENU_SIZE_26, 0, R.string.size_26);
+//                menu.add(0, MENU_SIZE_30, 0, R.string.size_30);
                 break;
         }
 
@@ -58,27 +64,33 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
 
-            case MENU_COLOR_RED :
+//            case MENU_COLOR_RED :
+            case R.id.text_color_red :
                 tvOutColor.setTextColor(Color.RED);
                 tvOutColor.setText(R.string.text_color_red);
                 break;
-            case MENU_COLOR_GREEN :
+//            case MENU_COLOR_GREEN :
+            case R.id.text_color_green :
                 tvOutColor.setTextColor(Color.GREEN);
                 tvOutColor.setText(R.string.text_color_green);
                 break;
-            case MENU_COLOR_BLUE :
+//            case MENU_COLOR_BLUE :
+            case R.id.text_color_blue :
                 tvOutColor.setTextColor(Color.BLUE);
                 tvOutColor.setText(R.string.text_color_blue);
                 break;
-            case MENU_SIZE_22 :
+//            case MENU_SIZE_22 :
+            case R.id.text_size_22 :
                 tvOutSize.setTextSize(22);
                 tvOutSize.setText(R.string.text_size_22);
                 break;
-            case MENU_SIZE_26 :
+//            case MENU_SIZE_26 :
+            case R.id.text_size_26 :
                 tvOutSize.setTextSize(26);
                 tvOutSize.setText(R.string.text_size_26);
                 break;
-            case MENU_SIZE_30 :
+//            case MENU_SIZE_30 :
+            case R.id.text_size_30 :
                 tvOutSize.setTextSize(30);
                 tvOutSize.setText(R.string.text_size_30);
                 break;
